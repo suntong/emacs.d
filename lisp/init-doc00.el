20ac2590934b0c59a565d1c67a7558a75da16e66
Tong Sun <suntong@cpan.org>
2015-01-10 10:46:17 -0500

    - [*] use markdown-toc only when in gfm-mode

--- lisp/init-doc00.el
+++ lisp/init-doc00.el
@@ -13,25 +13,27 @@
 
 ;;;_* markdown-mode
 (use-package markdown-mode
-  :mode ("\\.\\(txt\\|markdown\\|md\\)\\'" . gfm-mode))
-
-;; A [GitHub Flavored Markdown] (GFM) mode, `gfm-mode', is also
-;; available as part of markdown-mode. The most important differences
-;; are:
-;;
-;; - GFM code blocks, with optional programming language keywords,
-;;   will be highlighted. This is most important to me as the default
-;;   markdown-mode will not highlight them.
-;;
-;; - Underscores inside of words (such as test_variable) will not
-;;   trigger emphasis.
-
+  :mode ("\\.\\(txt\\|markdown\\|md\\)\\'" . gfm-mode)
+  ;; A [GitHub Flavored Markdown] (GFM) mode, `gfm-mode', is also
+  ;; available as part of markdown-mode. The most important differences
+  ;; are:
+  ;;
+  ;; - GFM code blocks, with optional programming language keywords,
+  ;;   will be highlighted. This is most important to me as the default
+  ;;   markdown-mode will not highlight them.
+  ;;
+  ;; - Underscores inside of words (such as test_variable) will not
+  ;;   trigger emphasis.
 
 ;;;_ > markdown-toc
-(use-package markdown-toc
-  :bind (("C-c C-x T" . markdown-toc/generate-toc)))
-;; It will compute the TOC at insert it at current position, but work only for github
-;; since those toc anchors are not inserted by default anywhere else.
+  :init
+  (progn
+    (use-package markdown-toc
+      :bind (("C-c C-x T" . markdown-toc/generate-toc)))
+    ;; It will compute the TOC at insert it at current position, but
+    ;; work only for github since those toc anchors are not inserted
+    ;; by default anywhere else.
+    ))
 
 ;;;_* others
 (load-library "init-doc00-doc-mode") 
