7047a06f96f0c66ac0c31da1ae42c06d78623ef8
Tong Sun <suntong@cpan.org>
2021-07-01 11:59:21 -0400

    - [#] add position-cursor to init-edit00_abbrev_test.el, and now it breaks

--- lisp/init-edit00_abbrev_test.el
+++ lisp/init-edit00_abbrev_test.el
@@ -28,6 +28,7 @@ Refer to the elisp comments in `abbrev--before-point' for details.")
     '(
 
       ("fo" "bar")
+      ("p" "print(▮)" ahf)
       ("`foo" "bar")
       ("`p" "print()" ahf)
       ;;
@@ -37,11 +38,52 @@ Refer to the elisp comments in `abbrev--before-point' for details.")
   (add-hook 'python-mode-hook #'abbrev-mode)
   )
 
+;; == adapted from http://ergoemacs.org/emacs/elisp_abbrev.html
+
 ;;  the “ahf” stand for abbrev hook function.
 (defun ahf ()
   "Abbrev hook function, used for `define-abbrev'.
 prevent inserting the char that triggered expansion."
   (interactive)
   t)
+
+(put 'ahf 'no-self-insert t)
+
+(setq abbrev-expand-function 'global-expand-abbrev)
+;; (setq abbrev-expand-function 'abbrev--default-expand)
+
+(defun global-expand-abbrev ()
+  "Function for value of `abbrev-expand-function'.
+Expand the symbol before cursor.
+Returns the abbrev symbol if there's a expansion, else nil."
+  (interactive)
+  (let ( $p1 $p2
+             $abrStr
+             $abrSymbol
+             )
+
+    (save-excursion
+      (forward-symbol -1)
+      (setq $p1 (point))
+      (forward-symbol 1)
+      (setq $p2 (point)))
+
+    (setq $abrStr (buffer-substring-no-properties $p1 $p2))
+    (setq $abrSymbol (abbrev-symbol $abrStr))
+    (if $abrSymbol
+        (progn
+          (abbrev-insert $abrSymbol $abrStr $p1 $p2 )
+          (global-abbrev-position-cursor $p1)
+          $abrSymbol)
+      nil)))
+
+(defun global-abbrev-position-cursor (&optional @pos)
+  "Move cursor (from @POS) back to ▮ if exist, else put at end.
+Return true if found, else false."
+  (interactive)
+  (let (($found-p (search-backward "▮" (if @pos @pos (max (point-min) (- (point) 100))) t )))
+    (when $found-p (delete-char 1))
+    $found-p
+    ))
 
 (setq save-abbrevs nil)
