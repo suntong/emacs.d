b8c937a46c78ff66faa22fb722a271b909b90e85
Tong Sun <suntong@cpan.org>
2021-07-20 09:53:23 -0400

    - [#] simplify to fix for Package cl is deprecated

--- load-path.el
+++ load-path.el
@@ -38,19 +38,4 @@
   (setq load-path
         (cons (expand-file-name path (or dir user-emacs-directory)) load-path)))
 
-(eval-when-compile (require 'cl))
-(defun sanityinc/add-subdirs-to-load-path (parent-dir)
-  "Adds every non-hidden subdir of PARENT-DIR to `load-path'."
-  (let* ((default-directory parent-dir))
-    (progn
-      (setq load-path
-            (append
-             (remove-if-not
-              (lambda (dir) (file-directory-p dir))
-              (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
-             load-path)))))
-
-(sanityinc/add-subdirs-to-load-path
- (expand-file-name "site-lisp/" user-emacs-directory))
-
 (provide 'load-path)
